a6065aee8e8ff5166dda43dffea62e0c3e8ca9a3
luanAfons0 <luan.hmatao@gmail.com>
2026-03-13 18:01:49 -0300

    update(knip): remove unused exports

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -4,5 +4,4 @@ export { Card } from "./Card/Card";
 export { Dialog } from "./Dialog/Dialog";
 export { Container } from "./Container/Container";
 export { Switch } from "./Switch/Switch";
-export { ThemeProvider, useTheme } from "./ThemeProvider/ThemeProvider";
 export { ThemeToggle } from "./ThemeToggle/ThemeToggle";
